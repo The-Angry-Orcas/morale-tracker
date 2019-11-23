64cc7c947698316bff8b8cebe33d02086969d39a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-23 17:27:56 +0000

    Deploy The-Angry-Orcas/morale-tracker to github.com/The-Angry-Orcas/morale-tracker.git:gh-pages

---
+++ precache-manifest.54848e7e0dc1f0c911a2c6be1730d9c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e67ec1966c0fe9d9e867e1bcf603f5",
+    "url": "/morale-tracker/index.html"
+  },
+  {
+    "revision": "154e9dd972acce928a0b",
+    "url": "/morale-tracker/static/css/main.ca180a33.chunk.css"
+  },
+  {
+    "revision": "550c02893e319fd0227f",
+    "url": "/morale-tracker/static/js/2.77ecf9d0.chunk.js"
+  },
+  {
+    "revision": "154e9dd972acce928a0b",
+    "url": "/morale-tracker/static/js/main.d8fc55c0.chunk.js"
+  },
+  {
+    "revision": "608f5e632506a17e435f",
+    "url": "/morale-tracker/static/js/runtime-main.c708e7f7.js"
+  }
+]);
